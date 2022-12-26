99766416d8f39d8263c8b165af0086934ff0c349
cchaae <kkang_chung125@yahoo.co.jp>
2022-12-26 17:45:34 +0900

    Fix rename

--- app/src/main/java/com/example/compose_practise/ui/theme/Theme.kt
+++ app/src/main/java/com/example/compose_practise/ui/theme/Theme.kt
@@ -28,7 +28,7 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun ComposepractiseTheme(
+fun ComposeArticleTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
